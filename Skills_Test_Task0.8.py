277e00aa7774f9f192661efbd4ad5043f880336b
MrGrumps <91214655+MrGrumps@users.noreply.github.com>
2021-09-30 09:35:07 +0200

    Add files via upload

--- Skills_Test_Task0.8.py
+++ Skills_Test_Task0.8.py
@@ -2,6 +2,21 @@
 def conv_time(x):
     hour = x // 60
     minutes = x % 60
-    print(str(hour) + " hours, " + str(minutes) + " minutes")
+    if hour == 0 and minutes < 2:
+        print(str(minutes) + " minute")
+    elif hour == 0 and minutes > 1:
+        print(str(minutes) + " minutes")
+    elif hour < 2 and minutes == 0:
+        print(str(hour) + " hour")
+    elif hour > 1 and minutes == 0:
+        print(str(hour) + " hours")
+    elif hour == 1 and minutes < 2:
+        print(str(hour) + " hour, " + str(minutes) + " minute")
+    elif hour == 1 and minutes > 1:
+        print(str(hour) + " hour, " + str(minutes) + " minutes")
+    elif hour > 1 and minutes < 2:
+        print(str(hour) + " hours, " + str(minutes) + " minute")
+    else:
+        print(str(hour) + " hours, " + str(minutes) + " minutes")
 
-conv_time(133)
+conv_time(61)
